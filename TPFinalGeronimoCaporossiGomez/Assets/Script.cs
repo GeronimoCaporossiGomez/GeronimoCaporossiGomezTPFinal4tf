9737c0b38a4a39f015d3c6a04257ca5d33c09a7a
GeronimoCaporossiGomez <geronimogomezok123@gmail.com>
2024-06-24 18:02:18 -0300

    a

---
+++ TPFinalGeronimoCaporossiGomez/Assets/Script.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script : MonoBehaviour
+{
+    public GameObject[] objects;
+    int currentIndex = -1;
+    // Start is called before the first frame update
+    void Start()
+    {
+       Desactivar();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            currentIndex++;
+            if (currentIndex >= objects.Length)
+            {
+                currentIndex = 0;
+            }
+           Desactivar();
+            ActivateByIndex(currentIndex);
+        }
+    }
+
+    void Desactivar()
+    {
+        for (int X = 0; X < objects.Length; X++)
+        {
+            objects[X].SetActive(false);
+        }
+    }
+    void ActivateByIndex(int index)
+    {
+        objects[index].SetActive(true);
+    }
+}
